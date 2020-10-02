cb3783c89311e9e93722d4adc124180dd97259b5
tmzkdl503 <50816812+tmzkdl503@users.noreply.github.com>
2020-10-02 16:04:41 +0900

    Create PL1-find_kim_in_seoul.py

---
+++ PL1-find_kim_in_seoul.py
@@ -0,0 +1,10 @@
+'''
+Title: 서울에서 김서방 찾기
+Type: 구현
+Link: https://programmers.co.kr/learn/courses/30/lessons/12919
+Reference: 연습문제
+'''
+
+def solution(seoul):
+    ans = seoul.index('Kim')
+    return f'김서방은 {ans}에 있다'
